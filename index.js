b324ad1c47cd3bcc7aea91e8e49311f7a77e0f11
lzq_Folder <41282608+lzq741167479@users.noreply.github.com>
2020-06-11 17:18:19 +0800

    node demo项目重构

--- index.js
+++ index.js
@@ -1,63 +1,43 @@
-// const Koa = require('koa');
-// const bodyParser = require('koa-bodyparser');
-// // const cors = require('koa-cors');
-// const app = new Koa();
-// //导入跨域配置中间件
-// const cors = require('./cors');
-// //导入controller的中间件
-// const controller = require('./controller');
-// //导入维护表间关联关系中间件
-// require('./relationship');
-// //导入自动建表中间件
-// const init_db = require('./init-db');
-
-// //自动建表
-// init_db.sync();
-
-
-// //注册跨域请求
-// app.use(cors());
-// //注册post请求体解析
-// app.use(bodyParser());
-// //注册所有路由
-// app.use(controller());
-// //监听8888端口
-// app.listen(8888);
-
 /**
  * @Author: 老林头
  * @Date: 2020/06/10
  * @Explain: 服务入口
  */
 
-const express = require('express')
-const app = express()
+var koa = require('koa')
+// 注意require('koa-router')返回的是函数:
+var router = require('koa-router')()
+var cors = require('koa2-cors') //跨域中间件
+var app = new koa()
 // 数据库服务
 const mysql = require('./mysql')
 
 //设置允许跨域访问该服务.
-app.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  //Access-Control-Allow-Headers ,可根据浏览器的F12查看,把对应的粘贴在这里就行
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Methods', '*');
-  res.header('Content-Type', 'application/json;charset=utf-8');
-  next();
+app.use(cors());
+
+// log request URL:
+app.use(async (ctx, next) => {
+  console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+  await next();
 });
 
-mysql.query("SELECT * FROM `world`.`city` WHERE `ID` = '10' LIMIT 0,1000", function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results);
+// add url-route:
+router.get('/hello/:name', async (ctx, next) => {
+  var name = ctx.params.name;
+  ctx.response.body = `<h1>Hello, ${name}!</h1>`;
 });
 
-app.get('/test', (req, res) => {
-  mysql.query("SELECT * FROM `world`.`city` WHERE `ID` = '10' LIMIT 0,10", function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results);
-    res.send(results)
-  });
-})
+router.get('/', async (ctx, next) => {
+  ctx.response.body = '<h1>Index</h1>';
+});
 
+// add router middleware:
+app.use(router.routes());
+
+// mysql.query("SELECT * FROM `world`.`city` WHERE `ID` = '10' LIMIT 0,1000", function (error, results, fields) {
+//   if (error) throw error;
+//   console.log('The solution is: ', results);
+// });
 app.listen(8078, () => {
     console.log('服务启动')
 })
